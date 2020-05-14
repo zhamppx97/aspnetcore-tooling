f5a7fb653c8dc5c0800b6bb17a36c4763688e496
Todd Grunke <toddgrun@microsoft.com>
2020-05-14 13:13:12 -0700

    First crack at removing razor editor factory

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/RazorEditorFactory.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/RazorEditorFactory.cs
@@ -2,25 +2,53 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.Composition;
 using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis.Razor;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Package;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
 
 namespace Microsoft.VisualStudio.LanguageServerClient.Razor
 {
+    [Export(typeof(IFilePathToContentTypeProvider))]
+    [Name(nameof(RazorLanguageServerFilePathToContentTypeProvider))]
+    [FileExtension(".cshtml")]
+    internal class RazorLanguageServerFilePathToContentTypeProvider : IFilePathToContentTypeProvider
+    {
+        private readonly IContentTypeRegistryService _contentTypeRegistryService;
+        private readonly LSPEditorFeatureDetector _lspEditorFeatureDetector;
+
+        [ImportingConstructor]
+        public RazorLanguageServerFilePathToContentTypeProvider(IContentTypeRegistryService contentTypeRegistryService, LSPEditorFeatureDetector lspEditorFeatureDetector)
+        {
+            _contentTypeRegistryService = contentTypeRegistryService;
+            _lspEditorFeatureDetector = lspEditorFeatureDetector;
+        }
+
+        public bool TryGetContentTypeForFilePath(string filePath, out IContentType contentType)
+        {
+            if (_lspEditorFeatureDetector.IsLSPEditorAvailable(filePath, hierarchy: null))
+            {
+                contentType = _contentTypeRegistryService.GetContentType(RazorLSPContentTypeDefinition.Name);
+                return true;
+            }
+
+            contentType = null;
+            return false;
+        }
+    }
+
     [Guid(EditorFactoryGuidString)]
     internal class RazorEditorFactory : EditorFactory
     {
         private const string EditorFactoryGuidString = "3dfdce9e-1799-4372-8aa6-d8e65182fdfc";
-        private readonly LSPEditorFeatureDetector _lspEditorFeatureDetector;
 
         public RazorEditorFactory(AsyncPackage package) : base(package)
         {
-            var componentModel = (IComponentModel)AsyncPackage.GetGlobalService(typeof(SComponentModel));
-            _lspEditorFeatureDetector = componentModel.GetService<LSPEditorFeatureDetector>();
         }
 
         public override int CreateEditorInstance(
@@ -36,29 +64,14 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
             out Guid cmdUI,
             out int cancelled)
         {
-            if (!_lspEditorFeatureDetector.IsLSPEditorAvailable(moniker, hierarchy))
-            {
-                docView = default;
-                docData = default;
-                editorCaption = null;
-                cmdUI = default;
-                cancelled = 0;
+            docView = default;
+            docData = default;
+            editorCaption = null;
+            cmdUI = default;
+            cancelled = 0;
 
-                // Razor LSP is not enabled, allow another editor to handle this document
-                return VSConstants.VS_E_UNSUPPORTEDFORMAT;
-            }
-
-            var editorInstance = base.CreateEditorInstance(createDocFlags, moniker, physicalView, hierarchy, itemid, existingDocData, out docView, out docData, out editorCaption, out cmdUI, out cancelled);
-            var textLines = (IVsTextLines)Marshal.GetObjectForIUnknown(docData);
-
-            // Next, the editor typically resets the ContentType after TextBuffer creation. We need to let them know
-            // to not update the content type because we'll be taking care of the ContentType changing lifecycle.
-            var userData = textLines as IVsUserData;
-            var hresult = userData.SetData(VSConstants.VsTextBufferUserDataGuid.VsBufferDetectLangSID_guid, false);
-
-            ErrorHandler.ThrowOnFailure(hresult);
-
-            return editorInstance;
+            // Razor LSP is not enabled, allow another editor to handle this document
+            return VSConstants.VS_E_UNSUPPORTEDFORMAT;
         }
     }
 }
