9dcab228cc7741d4249a06e1633f19c51fdc778f
N. Taylor Mullen <nimullen@microsoft.com>
2020-05-18 14:21:29 -0700

    WIP

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/FormattingContext.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/FormattingContext.cs
@@ -24,5 +24,38 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
         public Range Range { get; set; }
 
         public Dictionary<int, IndentationContext> Indentations { get; } = new Dictionary<int, IndentationContext>();
+
+        public string GetIndentationLevelString(int indentationLevel)
+        {
+            var indentChar = Options.InsertSpaces ? ' ' : '\t';
+            var indentationLength = indentationLevel;
+            if (Options.InsertSpaces)
+            {
+                indentationLength *= (int)Options.TabSize;
+            }
+            var indentation = new string(indentChar, indentationLength);
+            return indentation;
+        }
+
+        public string GetIndentationString(int indentation)
+        {
+            if (Options.InsertSpaces)
+            {
+                return new string(' ', indentation);
+            }
+            else
+            {
+                // Avoid writing directly to the StringBuilder here, that will throw off the manual indexing 
+                // done by the base class.
+                var tabs = indentation / Options.TabSize;
+                var tabPrefix = new string('\t', (int)tabs);
+
+                var spaces = indentation % Options.TabSize;
+                var spaceSuffix = new string(' ', (int)spaces);
+
+                var combined = string.Concat(tabPrefix, spaceSuffix);
+                return combined;
+            }
+        }
     }
 }
